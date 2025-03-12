cc2689453f5b03e03b07b1f8457631e6306f69e2
carlostousferragut <carlostousferragut@gmail.com>
2025-03-12 12:37:13 +0100

    Update EclipseIDE.ps1

--- EclipseIDE.ps1
+++ EclipseIDE.ps1
@@ -5,7 +5,7 @@ if (!(Test-Path $destinationFolder)) {
 }
 
 # URL oficial del instalador
-$installerUrl = "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2024-12/R/eclipse-jee-2024-12-R-win32-x86_64.zip"  # Puedes cambiarlo si hay nueva versión
+$installerUrl = "https://mirror.umd.edu/eclipse/technology/epp/downloads/release/2024-12/R/eclipse-jee-2024-12-R-win32-x86_64.zip"  # Puedes cambiarlo si hay nueva versión
 
 # Ruta destino del instalador
 $installerPath = "$destinationFolder\eclipse-jee-2024-12-R-win32-x86_64.zip"
@@ -22,7 +22,7 @@ Expand-Archive -Path $installerPath -DestinationPath $installPath -Force
 # Crear acceso directo para todos
 $wshell = New-Object -ComObject WScript.Shell
 $shortcut = $wshell.CreateShortcut("C:\Users\Public\Desktop\Eclipse IDE.lnk")
-$shortcut.TargetPath = "$installPath\eclipse.exe"
+$shortcut.TargetPath = "$installPath\eclipse\eclipse.exe"
 $shortcut.Save()
 
 # Limpiar
